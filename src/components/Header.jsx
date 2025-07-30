de3fe3b9cae1d815c496adffffc1987a38b535e7
soheilgoodarzi <soheilgoodarziii@gmail.com>
2025-07-30 23:12:04 +0330

    Refactor: Enhance responsiveness and styling in Header and Home components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,36 +6,35 @@ export default function Header() {
 
   return (
     <header>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
-          <div className="flex-shrink-0">
-            <h1 className="text-2xl font-bold text-white drop-shadow-md">
-              Taskify
-            </h1>
-          </div>
-          <nav className="flex items-center space-x-4">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                `${
-                  isActive ? activeLinkStyle : normalLinkStyle
-                } px-3 py-2 rounded-md  text-md font-medium transition-colors`
-              }
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/dashboard"
-              className={({ isActive }) =>
-                `${
-                  isActive ? activeLinkStyle : normalLinkStyle
-                } px-3 py-2 rounded-md text-md font-medium transition-colors`
-              }
-            >
-              Dashboard
-            </NavLink>
-          </nav>
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 2xl:px-16 flex justify-between items-center xs:h-12 md:h-16 2xl:h-32">
+        <div className="flex-shrink-0">
+          <h1 className="xs:text-xl sm:text-2xl md:text-2xl lg:text-3xl 2xl:text-6xl font-bold text-white drop-shadow-md">
+            Taskify
+          </h1>
         </div>
+
+        <nav className="flex items-center space-x-4">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${
+                isActive ? activeLinkStyle : normalLinkStyle
+              } px-3 py-2 rounded-md xs:text-xs sm:text-base md:text-lg lg:text-xl 2xl:text-4xl font-medium transition-colors`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              `${
+                isActive ? activeLinkStyle : normalLinkStyle
+              } px-3 py-2 rounded-md xs:text-xs sm:text-base md:text-lg lg:text-xl font-medium transition-colors`
+            }
+          >
+            Dashboard
+          </NavLink>
+        </nav>
       </div>
     </header>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom"
 export default function HomePage() {
   return (
-    <div className="flex h-[calc(100vh-64px)] flex-col items-center justify-center text-center text-white p-4 ">
-      <h1 className="text-5xl md:text-7xl font-bold mb-4 drop-shadow-lg">
+    <div className="flex lg:h-[calc(100vh-64px)] flex-col items-center justify-center text-center text-white p-4 ">
+      <h1 className="xs:text-2xl md:text-5xl lg:text-7xl 2xl:text-9xl font-bold md:mb-4 xs:mb-1 xs:mt-14 drop-shadow-lg">
         Welcome to Taskify
       </h1>
-      <p className="max-w-2xl text-lg md:text-xl mb-8 drop-shadow-md">
+      <p className="max-w-2xl xs:text-lg md:text-xl lg:text-2xl 2xl:text-3xl md:mb-6 lg:mb-8 2xl:mb-12 xs:mb-4 drop-shadow-md">
         A space to manage your tasks and boost your productivity.
       </p>
       <Link
         to="/dashboard"
-        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg text-lg transition-transform  transform hover:scale-105"
+        className="bg-blue-600 hover:bg-blue-700 text-white font-bold lg:py-2 lg:px-4 xs:px-2 xs:py-2 sm:py-2 sm:px-3 2xl:py-5 2xl:rounded-2xl 2xl:px-8 rounded-lg xs:text-sm md:text-lg lg:text-xl 2xl:text-4xl transition-transform transform hover:scale-105"
       >
         Go to Dashboard
       </Link>
